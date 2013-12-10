0b18a3478d7ee28861355f7dbb3cabf78537711a
Simone Dalla <simodalla@gmail.com>
2013-12-10 11:58:59 +0100

    add bootstrap

--- animacionlatinabologna/settings.py
+++ animacionlatinabologna/settings.py
@@ -197,6 +197,11 @@ STATIC_URL = "/static/"
 # Example: "/home/media/media.lawrence.com/static/"
 STATIC_ROOT = os.path.join(PROJECT_ROOT, STATIC_URL.strip("/"))
 
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, "assets"),
+)
+
+
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
